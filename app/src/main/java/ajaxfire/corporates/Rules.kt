e617d9a065fa14ade7ea40a82f923d0659b348dc
Ajax <40214600+Ajaxfire@users.noreply.github.com>
2018-06-14 02:58:48 +0530

    Second local push

--- app/src/main/java/ajaxfire/corporates/Rules.kt
+++ app/src/main/java/ajaxfire/corporates/Rules.kt
@@ -1,12 +1,20 @@
 package ajaxfire.corporates
 
+import ajaxfire.corporates.R.string.*
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import kotlinx.android.synthetic.main.activity_rules.*
 
 class Rules : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rules)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            textView2.text = (Html.fromHtml(ruleDetails.toString(), Html.FROM_HTML_MODE_COMPACT))
+        } else {
+            textView2.text = (Html.fromHtml(ruleDetails.toString()))
+        }
     }
 }
